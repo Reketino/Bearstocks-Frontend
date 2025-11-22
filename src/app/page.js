5be6ec2c56dece9aec663c899050fb8c7015880f
Reketino <bjornevensk8@gmail.com>
2025-11-22 16:23:11 +0100

    oppsettet på page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 "use client";
 import React from 'react'
 
+
+
 export default function Page() {
   return (
-    <div>
-      
-    </div>
-  )
+    <main className='min-h-screen bg-[#05060a] text-white p-6 flex flex-col gap-8 items-center'>
+      <Neonh1 text="BEARSTOCKS" />
+      <p className='opacity-70 text-sm'>Realtime stockfeed hustling stocks from bearstocks-backend.onrender.com</p>
+
+
+      <section className='w-full max-w-6xl'>
+        <LiveStocks />
+      </section>
+    </main>
+  );
 }
